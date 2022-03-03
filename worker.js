35a8c36bf8f2f9573404c68e70f9055819e129c5
Godwill Onyewuchi <g50021858@china.huawei.com>
2022-03-03 14:12:13 +0100

    changed redis url

--- worker.js
+++ worker.js
@@ -10,7 +10,7 @@ let maxJobsPerWorker = 50;
 
 
 function start() {
-  let workQueue = new Queue('work', REDIS_URL,
+  let workQueue = new Queue('work', 'redis://:p50ad4f338c54668a454a16b525cbb111631f1251bae387b41b2093cfa368539d@ec2-34-202-94-249.compute-1.amazonaws.com:32540',
   { redis: { tls: { rejectUnauthorized: false, requestCert: true,  }, maxRetriesPerRequest : 20,  }
  });
   workQueue.process(maxJobsPerWorker, async (job) => {
