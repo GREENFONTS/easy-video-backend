3ce7a0ff34e546ba18bf371568eb867085763941
Godwill Onyewuchi <g50021858@china.huawei.com>
2022-03-03 14:24:18 +0100

    edited response headers

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://localhost:3001']);
+  res.append('Access-Control-Allow-Origin', 'https://localhost:3001');
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
   next();
